df97b89e0a81980782eb073dadb708895084525c
Ulf Zackrisson <ulmezz@gmail.com>
2018-03-29 19:42:28 +0200

    Change static path in app.js

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'bin/www')));
 
 app.use('/users', userRoutes);
 app.use('/images', imageRoutes);
